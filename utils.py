f83100c34bf9b0b72c93876a9803ca015c69e36e
Francisco Cruz <jfbc.fis@gmail.com>
2026-06-07 07:13:56 -0600

    feat: add extract method to HandLandmarkExtractor

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import mediapipe as mp
 import numpy as np
+import cv2
 
 class HandLandmarkExtractor:
     def __init__(self):
@@ -9,3 +10,12 @@ class HandLandmarkExtractor:
             max_num_hands=1, #define el numero de manos. Más manos, más procesamiento.
             min_detection_confidence=0.7 #sólo reporta landmarks si está 70% seguro de haber detectado una mano
         )
+    def extract(self, frame):
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(frame_rgb)
+        
+        if results.multi_hand_landmarks:
+            landmarks = results.multi_hand_landmarks[0]
+            return np.array([[lm.x, lm.y, lm.z] 
+                            for lm in landmarks.landmark])
+        return None
